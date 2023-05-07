e65fffd162f60fd33f2818f39ea7bdb24dff4178
benben <z2266109@gmail.com>
2023-05-07 20:17:16 +0800

    feat(leetcode): add 2620 easy

---
+++ leetcode-easy/2620-counter.ts
@@ -0,0 +1,18 @@
+function createCounter(n: number): () => number {
+  return function() {
+    return n++
+  }
+}
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
+
+// 2023.05.07 Done.
+// Runtime 66 ms
+// Beats 29.11%
+// Memory 43.2 MB
+// Beats 42.52%
