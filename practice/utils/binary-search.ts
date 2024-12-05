afc91911b67bb2f24a9b3ec83e596719ca2b5ce2
benben <z2266109@gmail.com>
2024-12-05 21:38:56 +0800

    feat(practice): binary-search

---
+++ practice/utils/binary-search.ts
@@ -0,0 +1,24 @@
+export const arr = [1, 2, 5, 6, 10]
+
+console.log(binarySearch(arr, 5) === 2)
+console.log(binarySearch(arr, 1) === 0)
+console.log(binarySearch(arr, 10) === 4)
+console.log(binarySearch(arr, 0) === -1)
+console.log(binarySearch(arr, 20) === -1)
+
+function binarySearch(arr: number[], target: number) {
+  // code here
+  let start = 0
+  let end = arr.length
+  while (start < end) {
+    const mid = Math.floor((start + end) / 2)
+    if (arr[mid] === target) {
+      return mid
+    } else if (arr[mid] < target) {
+      start = mid + 1
+    } else {
+      end = mid
+    }
+  }
+  return -1
+}
