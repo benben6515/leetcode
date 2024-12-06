00f970135e6bba0c95e4d4a4337f928213ca80a2
benben <z2266109@gmail.com>
2024-12-06 21:00:53 +0800

    fix: binary-search

--- practice/utils/binary-search.ts
+++ practice/utils/binary-search.ts
@@ -9,15 +9,15 @@ console.log(binarySearch(arr, 20) === -1)
 function binarySearch(arr: number[], target: number) {
   // code here
   let start = 0
-  let end = arr.length
-  while (start < end) {
+  let end = arr.length - 1
+  while (start <= end) {
     const mid = Math.floor((start + end) / 2)
     if (arr[mid] === target) {
       return mid
     } else if (arr[mid] < target) {
       start = mid + 1
     } else {
-      end = mid
+      end = mid - 1
     }
   }
   return -1
