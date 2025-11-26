3a6128957df0dcbc35d82205e89198a3632970a9
benben <z2266109@gmail.com>
2025-11-26 13:01:14 +0800

    feat(leetcode-70): finish 0219

---
+++ leetcode-easy/0219-contains-duplicate-ii.ts
@@ -0,0 +1,37 @@
+export function containsNearbyDuplicate(nums: number[], k: number): boolean {
+  let left = 0
+  let right = Math.min(k, nums.length)
+
+  const hashMap = new Map()
+  for (let i = left; i < right; i++) {
+    const key = nums[i]
+    if (!hashMap.has(key)) {
+      hashMap.set(key, 0)
+    }
+    const value = hashMap.get(key)
+    if (value >= 1) return true
+    hashMap.set(key, value + 1)
+  }
+
+  while (right <= nums.length) {
+    const rightKey = nums[right]
+    if (!hashMap.has(rightKey)) {
+      hashMap.set(rightKey, 0)
+    }
+    const rightValue = hashMap.get(rightKey)
+    if (rightValue >= 1) return true
+    hashMap.set(rightKey, rightValue + 1)
+
+    const leftKey = nums[left]
+    const leftValue = hashMap.get(leftKey)
+    hashMap.set(leftKey, leftValue - 1)
+
+    left++
+    right++
+  }
+  return false
+}
+
+// 2025/11/26 done.
+// Runtime 38 ms Beats 27.51%
+// Memory 80.85 MB Beats 33.58%
