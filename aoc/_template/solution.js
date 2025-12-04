68a95f3f2f5eb049fd8986a8111ffdddfd8d31d0
benben <z2266109@gmail.com>
2025-12-04 15:39:14 +0800

    feat: update template

--- aoc/_template/solution.js
+++ aoc/_template/solution.js
@@ -6,12 +6,19 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 // Read input
-const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf-8')
+const input = fs.readFileSync(path.join(__dirname, 'example.txt'), 'utf-8')
+// const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf-8')
 const lines = input.trim().split('\n')
 
-function solution(lines) {
+function part1(lines) {
   // Your solution here
   return 0
 }
 
-console.log('Result: ', solution(lines))
+function part2(lines) {
+  // Your solution here
+  return 0
+}
+
+console.log('Part1 : ', solution(lines))
+console.log('Part2: ', solution(lines))
